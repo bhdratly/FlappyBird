4d0ad6535787ea784f2ae439b5c7ea5de98f10df
bhdratly <157316394+bhdratly@users.noreply.github.com>
2025-01-12 21:06:04 +0300

    FlappyBird
    Flappy Bird, basit ve eğlenceli bir arcade oyunudur. Oyuncu, ekrana dokunarak bir kuşu engellerden kaçırmaya çalışır ve en yüksek skoru elde etmeye hedefler. Oyunun amacı, kuşun engellerden geçmesini sağlamak ve uzun süre hayatta kalmaktır.
    
    Özellikler:
    
    Bağımlılık yapan basit oyun mekaniği.
    Engellerden kaçmak için hızlı refleksler ve dikkat gerektirir.
    Yüksek skor yapmak için mücadele.
    Teknolojiler:
    
    Dil: C#
    Platform: Visual Studio 2017

---
+++ FlappyBird/FlappyBird/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pipebottom = new System.Windows.Forms.PictureBox();
+            this.FlappyBird = new System.Windows.Forms.PictureBox();
+            this.pipetop = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pipebottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FlappyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipetop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pipebottom
+            // 
+            this.pipebottom.Image = global::FlappyBird.Properties.Resources.pipe;
+            this.pipebottom.Location = new System.Drawing.Point(542, 393);
+            this.pipebottom.Name = "pipebottom";
+            this.pipebottom.Size = new System.Drawing.Size(100, 223);
+            this.pipebottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipebottom.TabIndex = 0;
+            this.pipebottom.TabStop = false;
+            // 
+            // FlappyBird
+            // 
+            this.FlappyBird.Image = global::FlappyBird.Properties.Resources.bird;
+            this.FlappyBird.Location = new System.Drawing.Point(53, 216);
+            this.FlappyBird.Name = "FlappyBird";
+            this.FlappyBird.Size = new System.Drawing.Size(86, 60);
+            this.FlappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.FlappyBird.TabIndex = 1;
+            this.FlappyBird.TabStop = false;
+            // 
+            // pipetop
+            // 
+            this.pipetop.Image = global::FlappyBird.Properties.Resources.pipedown;
+            this.pipetop.Location = new System.Drawing.Point(709, -65);
+            this.pipetop.Name = "pipetop";
+            this.pipetop.Size = new System.Drawing.Size(100, 270);
+            this.pipetop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipetop.TabIndex = 2;
+            this.pipetop.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = global::FlappyBird.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(1, 591);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(897, 73);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 3;
+            this.ground.TabStop = false;
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.scoreText.Location = new System.Drawing.Point(83, 611);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(162, 46);
+            this.scoreText.TabIndex = 4;
+            this.scoreText.Text = "score:0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(891, 666);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipetop);
+            this.Controls.Add(this.FlappyBird);
+            this.Controls.Add(this.pipebottom);
+            this.Name = "Form1";
+            this.Text = "Flappy Bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pipebottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FlappyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipetop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pipebottom;
+        private System.Windows.Forms.PictureBox FlappyBird;
+        private System.Windows.Forms.PictureBox pipetop;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ FlappyBird/FlappyBird/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+        int pipspeed = 8; // Boru hızı
+        int gravity = 10; // Yer çekimi
+        int score = 0; // Skor
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Oyun döngüsü
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            FlappyBird.Top += gravity;
+            pipebottom.Left -= pipspeed;
+            pipetop.Left -= pipspeed;
+            scoreText.Text = "Score: " + score;
+
+            // Borular ekrandan çıkarsa sıfırlanır ve skor artar
+            if (pipebottom.Left < -150)
+            {
+                pipebottom.Left = 800;
+                score++;
+            }
+            if(pipetop.Left<-180)
+            {
+                pipetop.Left = 950;
+                score++;
+            }
+            // Kuş borulara veya yere çarparsa oyun biter
+            if (FlappyBird.Bounds.IntersectsWith(pipebottom.Bounds) ||
+                FlappyBird.Bounds.IntersectsWith(pipetop.Bounds) ||
+                 FlappyBird.Bounds.IntersectsWith(ground.Bounds) || FlappyBird.Top < -25
+                )
+            {
+                endGame();
+            }
+
+            // Skor 5'i geçerse boru hızı artar
+            if (score >5)
+            {
+                pipspeed = 15;
+            }
+        }
+
+        // Space tuşuna basıldığında kuş yukarı uçar
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Space)
+            {
+                gravity = -10;
+            }
+        }
+        // Space tuşu bırakıldığında kuş aşağı düşer
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 10;
+            }
+        }
+        // Oyun bitişi
+        private void endGame()
+        {
+            gameTimer.Stop();
+            scoreText.Text += " OYUN BİTTİ !!!";
+        }
+    }
+}
